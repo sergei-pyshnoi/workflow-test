228218f30ccedaaa44fdb974ec8db0bfddd05309
sergei-pyshnoi <sergei-pyshnoi@github.com>
2024-03-05 16:41:13 +0100

    Update main.m

--- main.m
+++ main.m
@@ -6,14 +6,21 @@ int main(int argc, const char * argv[]) {
         SecKeychainRef defaultKeychain = NULL;
         OSStatus status = SecKeychainCopyDefault(&defaultKeychain);
         if (status == errSecSuccess) {
-            CFStringRef keychainPath = NULL;
-            status = SecKeychainCopyPath(defaultKeychain, &keychainPath);
+            UInt32 pathLength = 0;
+            char *path = NULL;
+            status = SecKeychainGetPath(defaultKeychain, &pathLength, NULL);
             if (status == errSecSuccess) {
-                NSString *path = (__bridge NSString *)keychainPath;
-                NSLog(@"Default keychain path: %@", path);
-                CFRelease(keychainPath);
+                path = malloc(pathLength);
+                status = SecKeychainGetPath(defaultKeychain, &pathLength, path);
+                if (status == errSecSuccess) {
+                    NSString *keychainPath = [NSString stringWithUTF8String:path];
+                    NSLog(@"Default keychain path: %@", keychainPath);
+                } else {
+                    NSLog(@"Error getting default keychain path: %d", (int)status);
+                }
+                free(path);
             } else {
-                NSLog(@"Error getting default keychain path: %d", (int)status);
+                NSLog(@"Error getting default keychain path length: %d", (int)status);
             }
             CFRelease(defaultKeychain);
         } else {
